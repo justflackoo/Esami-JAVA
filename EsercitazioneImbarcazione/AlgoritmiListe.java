afb5f0b52cfb619a35df5a983b1ea60410b51f53
Giuseppe Celentano <giuseppecelentano4@gmail.com>
2024-07-02 16:00:33 +0200

    Update AlgoritmiListe.java

--- EsercitazioneImbarcazione/AlgoritmiListe.java
+++ EsercitazioneImbarcazione/AlgoritmiListe.java
@@ -7,128 +7,159 @@ package esercitazioneimbarcazione;
 public class ListaOrdinata {
     public class Nodo{
         private Imbarcazione data;
-        private Nodo next;
+        public Nodo next;
         
         public Nodo(Imbarcazione elem){
-            this.data = elem;
+            this.data=elem;
             this.next=null;
         }
     }
     Nodo testa;
-    public ListaOrdinata(){this.testa = null;}
-    
+    public ListaOrdinata(){this.testa=null;}
     public boolean isEmpty(){return this.testa==null;}
     public boolean isFull(){return false;}
     
-    //Inserimento in testa
     public void push(Imbarcazione i){
         Nodo q = new Nodo(i);
-        q.next = testa;
-        testa = q;
+        q.next=testa;
+        testa=q;
     }
-
-    //Inserimento in coda
+    
     public void append(Imbarcazione i){
-        if(isEmpty()){push(i);
+        if(isEmpty()){
+            push(i);
         }else{
             Nodo temp = testa;
             Nodo q = new Nodo(i);
             while(temp.next!=null){
                 temp = temp.next;
             }
-            temp.next = q;
+            temp.next=q;
         }
     }
-
-    //Inserimento secondo l'ordine definito dal compareTo
-    public void insert(Imbarcazione i ){
+    
+    public void insertCompareTo(Imbarcazione i){
         if(isEmpty() || testa.data.compareTo(i)>0){
             push(i);
         }else{
-            Nodo q = new Nodo(i);
             Nodo temp = testa;
+            Nodo q = new Nodo(i);
             while(temp.next!=null && temp.next.data.compareTo(i)<0){
                 temp = temp.next;
-            }
-            q.next=temp.next;
-            temp.next = q;
         }
+            q.next=temp.next;
+            temp.next=q;
     }
-
-    //Rimuovi in testa
+    }
+    
+    public void insert(Imbarcazione i){
+        if(isEmpty() || testa.data.getStazza()>i.getStazza()){
+            push(i);
+        }
+        
+        Nodo temp = testa;
+        Nodo q = new Nodo(i);
+        while(temp.next!=null && temp.next.data.getStazza()<i.getStazza()){
+            temp = temp.next;
+        }
+        q.next=temp.next;
+        temp.next=q;
+    }
+    
     public Imbarcazione pop(){
-        Imbarcazione imb = testa.data;
+        Imbarcazione i = testa.data;
         testa = testa.next;
-        return imb;
+        return i;
     }
-
-    //Rimuovi in coda
+    
     public Imbarcazione pop_back(){
-        if(testa.next==null){return pop();
+        if(testa.next==null){
+            return pop();
         }else{
             Nodo temp = testa;
             while(temp.next.next!=null){
                 temp = temp.next;
             }
-            Imbarcazione imb = temp.next.data;
-            temp.next = null;
-            return imb;
+            Imbarcazione i = temp.next.data;
+            temp.next=null;
+            return i;
         }
     }
-
-    //Rimuovi prima occorrenza
-    public void delete(Imbarcazione imb){
-        if(isEmpty()){System.out.println("Lista vuota");}
+    
+    public void elimina(Imbarcazione i){
+        if(isEmpty()){
+            System.out.println("Lista vuota");
+        }
+            Nodo temp = testa;
+            while(!temp.next.data.equals(i)){
+                temp = temp.next;
+            }
+            temp.next=temp.next.next;
         
-        Nodo temp = testa;
-        while(!temp.next.data.equals(imb)){
-            temp = temp.next;
-        }
-        if(temp.next==null){System.out.println("Elemento non trovato");}
-        temp.next = temp.next.next;
     }
-
-    //Ricerca di un elemento
-    public boolean inLista(Imbarcazione imb){
+    
+    public Imbarcazione top(){
+        Imbarcazione i = testa.data;
+        return i;
+    }
+    
+    public Imbarcazione top_back(){
+        if(testa.next==null){
+            return top();
+        }else{
+            Nodo temp = testa;
+            while(temp.next.next!=null){
+                temp = temp.next;
+        }
+            Imbarcazione i=temp.next.data;
+            return i;
+    }
+    }
+    
+    public boolean inLista(Imbarcazione i){
         boolean trovato = false;
         Nodo temp = testa;
         while(temp!=null && !trovato){
-            if(temp.data.equals(imb)){
-                trovato = true;
+            if(temp.data.equals(i)){
+               trovato = true; 
             }else{
-                temp = temp.next;
+            temp = temp.next;
             }
         }
         return trovato;
     }
-
-    //Ricerca del massimo 
-    public Imbarcazione ricercaMassimo(){
+    
+    public Imbarcazione ricercaMax(){
         Imbarcazione max = testa.data;
         Nodo temp = testa;
         while(temp.next!=null){
-            if(temp.data.getNumPasseggeri()>=max.getNumPasseggeri()){
+            if(temp.data.getStazza()>=max.getStazza()){
                 max = temp.data;
             }
-                temp = temp.next;
-            
+            temp = temp.next;
         }
         return max;
     }
-
-    //Ricerca del minimo
-    public Imbarcazione ricercaMinimo(){
+    
+    public Imbarcazione ricercaMin(){
         Imbarcazione min = testa.data;
         Nodo temp = testa;
         while(temp.next!=null){
-            if(temp.data.getNumPasseggeri()<=min.getNumPasseggeri()){
+            if(temp.data.getStazza()<=min.getStazza()){
                 min = temp.data;
             }
             temp = temp.next;
         }
         return min;
     }
-            
+    
+    public void stampa(){
+        Nodo temp = testa;
+        while(temp!=null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    } 
     
     /* PER TIPI PRIMITIVI   
     public boolean isOrdered () {
